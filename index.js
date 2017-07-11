3fa8da74b78d76ed4be43ac2568a6b0054822cde
Mitchell Patin <mitchell.patin@gmail.com>
2017-07-10 21:27:17 -0400

    added responses counting to eventsView

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.get('/events', function(req, res) {
 
   function onScan(err, data) {
     if (err) {
-      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+      console.error('Unable to scan the table. Error JSON:', JSON.stringify(err, null, 2));
     } else {
-      console.log("Scan succeeded.");
+      console.log('Scan succeeded.');
       var events = [];
       data.Items.forEach(function(event) {
         events.push(event);
@@ -56,17 +56,17 @@ app.get('/events', function(req, res) {
 app.get('/events/:id', function(req, res) {
 
   var params = {
-    TableName: "Events",
+    TableName: 'Events',
     Key: {
-      "id": req.params.id
+      'id': req.params.id
     }
   };
 
   docClient.get(params, function(err, data) {
     if (err) {
-      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2));
     } else {
-      console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+      console.log('GetItem succeeded:', JSON.stringify(data, null, 2));
       res.render('eventView', {
         event: data.Item
       });
@@ -78,31 +78,31 @@ app.post('/events/create', function(req, res) {
   // *************************
   // need to add check here to make sure id doesn't already exist
   // *************************
-  let id = randomstring.generate({
+  var id = randomstring.generate({
     length: 3,
     capitalization: 'lowercase'
   });
 
   console.log(id);
   var params = {
-    TableName: "Events",
+    TableName: 'Events',
     Item: {
-      "id": id,
-      "name": req.body.name,
-      "location": req.body.location,
-      "time": req.body.time,
-      "creator": req.body.creator,
-      "participants": [{
-        "name": req.body.creator,
-        "io": "In"
+      'id': id,
+      'name': req.body.name,
+      'location': req.body.location,
+      'time': req.body.time,
+      'creator': req.body.creator,
+      'participants': [{
+        'name': req.body.creator,
+        'io': 'In'
       }]
     }
   };
   docClient.put(params, function(err, data) {
     if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
     } else {
-      console.log("Added item:", JSON.stringify(data, null, 2));
+      console.log('Added item:', JSON.stringify(data, null, 2));
       res.redirect('/events');
     }
   });
@@ -110,26 +110,25 @@ app.post('/events/create', function(req, res) {
 
 app.post('/events/respond/:id', function(req, res) {
   console.log(req.body.comments);
+  var newResponse;
   if (req.body.comments === '') {
-    var newResponse = {
-      "name": req.body.name,
-      "io": req.body.io
-    }
+    newResponse = {
+      'name': req.body.name,
+      'io': req.body.io
+    };
   }
   else {
-    var newResponse = {
-      "name": req.body.name,
-      "io": req.body.io,
-      "comments": req.body.comments
-    }
+    newResponse = {
+      'name': req.body.name,
+      'io': req.body.io,
+      'comments': req.body.comments
+    };
   }
-
-  console.log(newResponse)
 
   var params = {
     TableName: 'Events',
     Key: {
-      "id": req.params.id,
+      'id': req.params.id,
     },
     ReturnValues: 'ALL_NEW',
     UpdateExpression: 'set #participants = list_append(#participants, :newResponse)',
@@ -139,14 +138,14 @@ app.post('/events/respond/:id', function(req, res) {
     ExpressionAttributeValues: {
       ':newResponse': [newResponse]
     }
-  }
+  };
 
-  console.log("Updating the item...");
+  console.log('Updating the item...');
   docClient.update(params, function(err, data) {
     if (err) {
-      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error('Unable to update item. Error JSON:', JSON.stringify(err, null, 2));
     } else {
-      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+      console.log('UpdateItem succeeded:', JSON.stringify(data, null, 2));
       res.redirect('/events/'+req.params.id);
     }
   });
